63012e8dc2d7f6b33cafa505734b236ea562d6de
luis jose sanchez carrasco <luissancar@yahoo.es>
2023-01-17 10:27:36 +0100

    ff

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -4,8 +4,14 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
+
+
+    public GameObject efectoExplosion;
     private void OnCollisionEnter(Collision collision)
     {
+
+        Instantiate(efectoExplosion, transform.position,
+            transform.rotation);
         if (collision.gameObject.CompareTag("Destruir"))
         {
             Destroy(collision.gameObject);
--- Assets/Scripts/LanzarGranada.cs
+++ Assets/Scripts/LanzarGranada.cs
@@ -12,7 +12,7 @@ public class LanzarGranada : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.G))
+        if (Input.GetKeyDown(KeyCode.G) || Input.GetButtonDown("Fire2"))
         {
             Lanzar();
         }
